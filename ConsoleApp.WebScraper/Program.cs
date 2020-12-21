b59c881344fa2637af2fd4c984f6db2539832b49
IevaMo <mockute1@gmail.com>
2020-12-21 15:23:19 +0100

    Add project files.

---
+++ ConsoleApp.WebScraper/Program.cs
@@ -0,0 +1,28 @@
+﻿using HtmlAgilityPack;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.API
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var httpClient = new HttpClient();
+
+            var response = await httpClient.GetAsync("https://www.cvonline.lt/darbo-skelbimai/informacines-technologijos");
+
+            var responseBody = await response.Content.ReadAsStringAsync();
+
+            var htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(responseBody);
+
+            var links = htmlDoc.DocumentNode.Descendants("div")
+                .Where(node => node.GetAttributeValue("class", "").Contains("offer_primary_info")).ToList();
+
+            var children = links.Select(l => l.FirstChild.FirstChild.InnerHtml);
+
+        }
+    }
+}
